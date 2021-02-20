0dd0a9a0f09d222681f0fde38f0870295360c404
alejoalzate <alejoalzate12@gmail.com>
2021-02-20 09:12:53 -0500

    Precompile assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   before_create :create_activation_digest
   
   has_many :microposts, dependent: :destroy
-
+  has_many :active_relationships
+  
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
