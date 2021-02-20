b704e32eac4143ec6e50de80701f8fe04ee9b709
alejoalzate <alejoalzate12@gmail.com>
2021-02-20 10:35:45 -0500

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new' 
   post '/login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   get 'help', to: 'welcome#help'
   get 'about', to: 'welcome#about'
@@ -15,4 +19,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
