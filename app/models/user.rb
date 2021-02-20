820da6c7eec9cb8f953b8298321d411219e96430
alejoalzate <alejoalzate12@gmail.com>
2021-02-20 09:21:58 -0500

    Precompile assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   before_create :create_activation_digest
   
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id",   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -69,6 +71,16 @@ class User < ApplicationRecord
   
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
